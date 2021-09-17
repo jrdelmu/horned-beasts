ae48c2e75d26917793ee27ef16a4572328c57310
Jovincent Del Mundo <jovincent.delmundo@gmail.com>
2021-09-16 22:48:33 -0700

    separating components and starting modal

---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react"
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+
+class Header extends Component {
+  render(){
+    return (  <Navbar bg="dark" variant="dark">
+    <Container>
+      <Navbar.Brand href="#home">
+        {this.props.title}
+      </Navbar.Brand>
+    </Container>
+  </Navbar>
+    )
+  }
+}
+
+export default Header;
